304d84110191af026218285674ceaace61835c68
Bigyan Thanait <bigyanthanait8@gmail.com>
2023-01-25 14:14:23 +0545

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./build/*.html'],
+  theme: {
+    fontFamily:{
+      'inconsolata':['Inconsolata', 'monospace'],
+      'spacemono':['Space Mono', 'monospace'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
